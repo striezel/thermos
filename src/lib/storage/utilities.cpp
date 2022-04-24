65f112e788b6a63bdd3b4e06123ffaae9f78ece0
Dirk Stolle <striezel-dev@web.de>
2022-04-25 01:32:18 +0200

    add workaround for missing localtime_r() function on MSVC
    MSVC has localtime_s() instead, and the order of the parameters
    is inverted there, compared to localtime_r().

--- src/lib/storage/utilities.cpp
+++ src/lib/storage/utilities.cpp
@@ -27,14 +27,23 @@ namespace thermos::storage
 nonstd::expected<std::string, std::string> time_to_string(const device_reading::reading_time_t& date_time)
 {
   const std::time_t tt = std::chrono::system_clock::to_time_t(date_time);
+  struct tm tm;
+  #if !defined(_MSC_VER)
   // Note: localtime() is NOT thread-safe. Therefore we use localtime_r(), which
   // is thread-safe but may not be available on all platforms or compilers.
-  struct tm tm;
   struct tm* ptr = localtime_r(&tt, &tm);
   if (ptr == nullptr)
   {
     return nonstd::make_unexpected("Date conversion with localtime_r() failed!");
   }
+  #else
+  // MSVC does not have localtime_r, so we use localtime_s instead.
+  const errno_t error = localtime_s(&tm, &tt);
+  if (error != 0)
+  {
+    return nonstd::make_unexpected("Date conversion with localtime_s() failed!");
+  }
+  #endif
   const int realYear = tm.tm_year + 1900;
   const int realMonth = tm.tm_mon + 1;
   return // zero-padding for year
